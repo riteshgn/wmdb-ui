4c13dada30f6a90bc779dd70db2b501ad30f9132
Ritesh Nair <riteshgn@gmail.com>
2020-11-23 19:36:49 -0500

    Convert MovieFinder to a controlled component

--- src/components/showcase/movie-finder/movie-finder.jsx
+++ src/components/showcase/movie-finder/movie-finder.jsx
@@ -1,37 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-import debounce from 'lodash/debounce';
-
-import SearchEngine from './movie-finder.search-engine';
+import { useSearch } from './movie-finder.hooks';
 import Styles from './movie-finder.module.scss';
 
-// Will delay function invocation until this time has elapsed
-const DEBOUNCE_WAIT_TIME_IN_MS = 225;
-// Max time function is allowed to be delayed
-// If a new invocation does not occur by this time then
-// the function will be invole with previous arguments
-const DEBOUNCE_MAX_WAIT_TIME_IN_MS = 500;
-
-/**
- * The search bar triggers multiple search requests to the backend and not all
- * of them are necessary. The entry when user pauses typing is the most crucial.
- *
- * So the debounce API is used here to rate limit the number of times a request
- * is sent to the search engine.
- *
- * @see https://lodash.com/docs/4.17.15#debounce
- */
-function _debouncedSearch(setResults) {
-    return debounce(
-        (e) => SearchEngine.search(e.target.value, setResults),
-        DEBOUNCE_WAIT_TIME_IN_MS,
-        { maxWait: DEBOUNCE_MAX_WAIT_TIME_IN_MS }
-    );
-}
-
 export default function MovieFinder() {
-    const [results, setResults] = useState([]);
+    const { searchString, searchResults, setSearchString } = useSearch();
 
     return (
         <div className={`dropdown is-active ${Styles.w100}`}>
@@ -40,13 +14,14 @@ export default function MovieFinder() {
                     className="input"
                     type="text"
                     placeholder="Search for a Movie or TV Show..."
-                    onChange={_debouncedSearch(setResults)}
+                    value={searchString}
+                    onChange={(e) => setSearchString(e.target.value)}
                 />
             </div>
-            {Boolean(results.length) && <div className={`dropdown-menu ${Styles.w100}`} role="menu">
+            {Boolean(searchResults.length) && <div className={`dropdown-menu ${Styles.w100}`} role="menu">
                 <div className="dropdown-content">
                     {
-                        results.map((result, index) => (
+                        searchResults.map((result, index) => (
                             <div key={index} className="dropdown-item">
                                 <Link to={`/${result.type}/${result.id}`}>
                                     <p>
